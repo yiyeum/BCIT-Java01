868fe42138915364d6867d0282a1b55981680f6d
yiyeum <KateYeEumLee@iyeeums-air.wifi.bcit.ca>
2017-04-23 14:15:21 -0700

    Recall from the last lab.

---
+++ Java01-Lab09/src/Card.java
@@ -0,0 +1,47 @@
+/**
+ * @author KateYeEumLee
+ * @version 1.9
+ */
+public class Card {
+	private String suit;
+	private String description;
+	
+	public Card(){}
+	public Card(String description, String suit) {
+		setDescription(description);
+		setSuit(suit);
+	}
+
+	/**
+	 * Method to get the suit.
+	 * @return the suit as a String.
+	 */
+	public String getSuit() {
+		return suit;
+	}
+
+	/**
+	 * Method to set the suit.
+	 * @param suit the suit to set
+	 */
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	/**
+	 * Method to get the description.
+	 * @return the description as a String.
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Method to set the description.
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
